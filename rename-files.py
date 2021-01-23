a3b975f5e40ab5ec1b3d00186f8c1387bc53de58
schmelto <tom.schmelzer@web.de>
2021-01-23 17:43:33 +0100

    update with png

--- rename-files.py
+++ rename-files.py
@@ -1,18 +1,30 @@
 import os 
 
-url = "url" + "/"
-extensions = ('.jpg', '.JPG')
+url = "" + "/"
+newFilename = ""
+extensions1 = ('.jpg', '.JPG')
+extensions2 = ('.png', '.PNG')
 
 # Function to rename multiple files 
 def main(): 
+    counter = 1
     for count, file in enumerate(os.listdir(url)): 
-        if file.endswith(extensions):
-            dst = "Filename" + str(count) + ".jpg"
+        if file.endswith(extensions1):
+            dst = newFilename + str(counter) + ".jpg"
             src = url + file 
             dst = url + dst
-            
             # rename() function will rename all the files 
-            os.rename(src, dst) 
+            os.rename(src, dst)
+            counter = counter + 1  
+        elif file.endswith(extensions2):
+            dst = newFilename + str(counter) + ".png"
+            src = url + file 
+            dst = url + dst
+            # rename() function will rename all the files 
+            os.rename(src, dst)
+            counter = counter + 1 
+        
+          
 
 # Driver Code 
 if __name__ == '__main__': 
